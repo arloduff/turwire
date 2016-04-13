f99a6dd84a3f08b5e564f8c48468ce01c9839fe4
Arlo Duff <aduff@nyu.edu>
2016-04-13 14:37:09 -0400

    Removed console.log

--- src/js/index.js
+++ src/js/index.js
@@ -3,8 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Main';
 
-console.log("Render");
-console.log("ReactDOM", ReactDOM);
 $(document).ready(function () {
   ReactDOM.render(<Main />, document.getElementById('main'));
 });
