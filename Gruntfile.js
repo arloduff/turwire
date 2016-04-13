bd651e2630264b931d6a0ca1c12233394b72f208
Arlo Duff <aduff@nyu.edu>
2016-04-13 03:32:46 -0400

    Cleaned up Gruntfile and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,44 +1,13 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    'jshint-jsx': {
-      options: {
-	esnext: true,
-	convertJSX: "js",
-	globals: [
-	  "React"
-	]
-      },
-      files: {
-	src: ['src/*.js']
-      }
-    },
-    copy: {
+   pkg: grunt.file.readJSON('package.json'),
+   copy: {
       main: {
 	cwd: 'src/html',
 	src: '*.html',
 	dest: 'dist/',
 	expand: true
      }
-    },
-    concat: {
-      options: {
-	separator: ";"
-      },
-      dist: {
-        src: ['src/*.js'],
-        dest: 'build/app.js'
-      }
-    },
-    babel: {
-      options: {
-        presets: ['es2015', 'react']
-      },
-      dist: {
-        files: {
-          'build/app.js': 'build/app.js'
-        }
-      }
     },
     browserify: {
       dist: {
@@ -68,23 +37,17 @@ module.exports = function (grunt) {
     }
  });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint-jsx');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-babel');
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.registerTask('default', [
-//    'jshint-jsx',
     'copy',
-    'concat',
-//    'babel',
     'browserify',
-    'sass',
     'uglify',
+    'sass',
     'cssmin'
   ]);
 };
